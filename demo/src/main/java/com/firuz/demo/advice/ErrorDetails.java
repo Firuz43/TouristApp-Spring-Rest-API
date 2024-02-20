37297b60afeb687164ce44c6b1350ac917863df4
Firuz43 <firuz1199@mail.ru>
2024-02-20 20:20:33 +0400

    Exceptions package updates

--- demo/src/main/java/com/firuz/demo/advice/ErrorDetails.java
+++ demo/src/main/java/com/firuz/demo/advice/ErrorDetails.java
@@ -6,4 +6,40 @@ public class ErrorDetails {
     private String statusCode;
     private String msg;
     private LocalDateTime when;
+
+    public String getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(String statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public LocalDateTime getWhen() {
+        return when;
+    }
+
+    public void setWhen(LocalDateTime when) {
+        this.when = when;
+    }
+
+    public ErrorDetails(String statusCode, String msg, LocalDateTime when) {
+        this.statusCode = statusCode;
+        this.msg = msg;
+        this.when = when;
+    }
+
+    @Override
+    public String toString() {
+        return "ErrorDetails [statusCode=" + statusCode + ", msg=" + msg + ", when=" + when + "]";
+    }
+
 }
--- demo/src/main/java/com/firuz/demo/advice/TouristErrorController.java
+++ demo/src/main/java/com/firuz/demo/advice/TouristErrorController.java
@@ -1,14 +1,31 @@
 package com.firuz.demo.advice;
 
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import com.firuz.demo.exception.TouristNotFoundException;
+
 @RestControllerAdvice
 public class TouristErrorController {
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<?> handleExceptionGlobally(Exception e) {
+    @ExceptionHandler(TouristNotFoundException.class)
+    public ResponseEntity<ErrorDetails> handleExceptionTourist(TouristNotFoundException e) {
 
+        System.out.println("Advice TouristExceptionHandler");
+        ErrorDetails error = new ErrorDetails("404 NOT FOUND", e.getMessage(), LocalDateTime.now());
+
+        return new ResponseEntity<ErrorDetails>(error, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDetails> handleExceptionGlobally(Exception e) {
+
+        ErrorDetails error = new ErrorDetails("404 NOT FOUND", e.getMessage(), LocalDateTime.now());
+
+        return new ResponseEntity<ErrorDetails>(error, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
