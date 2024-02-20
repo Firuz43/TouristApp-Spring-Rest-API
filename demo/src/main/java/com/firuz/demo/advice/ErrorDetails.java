6d95a20e37c38af14fb095743094a5f5c426a2b3
Firuz43 <firuz1199@mail.ru>
2024-02-20 18:12:04 +0400

    error class for handling exceptions

---
+++ demo/src/main/java/com/firuz/demo/advice/ErrorDetails.java
@@ -0,0 +1,9 @@
+package com.firuz.demo.advice;
+
+import java.time.LocalDateTime;
+
+public class ErrorDetails {
+    private String statusCode;
+    private String msg;
+    private LocalDateTime when;
+}
--- demo/src/main/java/com/firuz/demo/advice/TouristErrorController.java
+++ demo/src/main/java/com/firuz/demo/advice/TouristErrorController.java
@@ -1,8 +1,14 @@
 package com.firuz.demo.advice;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class TouristErrorController {
 
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleExceptionGlobally(Exception e) {
+
+    }
 }
