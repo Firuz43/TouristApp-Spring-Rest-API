a4245c8147482ac1f0df03c4492d6e8b3266493e
Firuz Chamarov <firuz1199@mail.ru>
2024-07-04 07:18:57 +0400

    saving api service

--- demo/src/main/java/com/firuz/demo/service/ClientManagement.java
+++ demo/src/main/java/com/firuz/demo/service/ClientManagement.java
@@ -1,5 +1,7 @@
 package com.firuz.demo.service;
 
+import com.firuz.demo.entity.Client;
+
 public interface ClientManagement {
-    
+    public String saveClient(Client client);
 }
--- demo/src/main/java/com/firuz/demo/service/ClientManagemetImpl.java
+++ demo/src/main/java/com/firuz/demo/service/ClientManagemetImpl.java
@@ -1,5 +1,15 @@
 package com.firuz.demo.service;
 
+import com.firuz.demo.entity.Client;
+import com.firuz.demo.repository.ClientRepo;
+
 public class ClientManagemetImpl implements ClientManagement {
+
+    private ClientRepo clientRepository;
+
+    @Override
+    public String saveClient(Client client) {
+        return "Client saved" + clientRepository.save(client);
+    }
     
 }
