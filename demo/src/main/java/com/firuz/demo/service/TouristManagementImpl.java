11f007415e0aa8c5dbbac31219a180c3ef592fab
Firuz Chamarov <firuz1199@mail.ru>
2024-07-27 05:39:41 +0400

    chh

--- demo/src/main/java/com/firuz/demo/service/TouristManagementImpl.java
+++ demo/src/main/java/com/firuz/demo/service/TouristManagementImpl.java
@@ -32,7 +32,7 @@ public class TouristManagementImpl implements TouristManagement {
                 .orElseThrow(() -> new TouristNotFoundException("Tourist with  id " + id + " not found"));
     }
 
-    // Fetching list of tourists from database the new change
+    // Fetching list of tourists from database the new changes
     @Override
     public List<Tourist> fetchAllTourist() {
         List<Tourist> list = touristRepository.findAll();
