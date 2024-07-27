e943069926f669fc48ebdd6b7f0e05e76cee8dda
Firuz Chamarov <firuz1199@mail.ru>
2024-07-27 06:22:09 +0400

    cc

--- demo/src/main/java/com/firuz/demo/repository/StudentRepo.java
+++ demo/src/main/java/com/firuz/demo/repository/StudentRepo.java
@@ -1,5 +1,11 @@
 package com.firuz.demo.repository;
 
-public interface StudentRepo {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.firuz.demo.entity.Student;
+
+@Repository
+public interface StudentRepo extends JpaRepository<Integer, Student>{
     
 }
