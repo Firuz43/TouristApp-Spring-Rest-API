7bacbb84956e4f72200eac49c27087140909250c
Firuz Chamarov <firuz1199@mail.ru>
2024-07-31 12:40:09 +0400

    saving api corrected

--- demo/src/main/java/com/firuz/demo/controller/StudentController.java
+++ demo/src/main/java/com/firuz/demo/controller/StudentController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.firuz.demo.entity.Student;
@@ -21,7 +22,7 @@ public class StudentController {
 
 
     @PostMapping("/save")
-    public ResponseEntity<String> saveStudent(Student student) {
+    public ResponseEntity<String> saveStudent(@RequestBody Student student) {
         String msg = studentService.registerStudent(student);
 
         return new ResponseEntity<>(msg, HttpStatus.OK);
