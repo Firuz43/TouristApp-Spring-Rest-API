ee4c6a1539ea6ea74b04284dc927521b68d72f78
Firuz Chamarov <firuz1199@mail.ru>
2024-08-07 06:16:49 +0400

    changes

--- demo/src/main/java/com/firuz/demo/advice/StudentErrorController.java
+++ demo/src/main/java/com/firuz/demo/advice/StudentErrorController.java
@@ -1,8 +1,15 @@
 package com.firuz.demo.advice;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class StudentErrorController {
     
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleExceptionsGlobally(Exception e) {
+        
+    }
 }
