1d7b30a054f9b0bb8099d63d50064545eb1fe37a
Firuz Chamarov <firuz1199@mail.ru>
2024-08-07 06:24:37 +0400

    exception

--- demo/src/main/java/com/firuz/demo/advice/StudentErrorController.java
+++ demo/src/main/java/com/firuz/demo/advice/StudentErrorController.java
@@ -1,5 +1,8 @@
 package com.firuz.demo.advice;
 
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -10,6 +13,8 @@ public class StudentErrorController {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> handleExceptionsGlobally(Exception e) {
-        
+        ErrorDetails error = new ErrorDetails("404 not found", e.getMessage(), LocalDateTime.now());
+
+        return new ResponseEntity<ErrorDetails>(error, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
