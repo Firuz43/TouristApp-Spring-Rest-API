e250825aa46bbfe0328957dedc058d4b0fc1c381
Firuz Chamarov <firuz1199@mail.ru>
2024-08-08 12:02:29 +0400

    making global exception

--- demo/src/main/java/com/firuz/demo/advice/StudentErrorController.java
+++ demo/src/main/java/com/firuz/demo/advice/StudentErrorController.java
@@ -3,6 +3,7 @@ package com.firuz.demo.advice;
 import java.time.LocalDateTime;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -20,5 +21,13 @@ public class StudentErrorController {
         ErrorDetails error = new ErrorDetails("404 not found", e.getMessage(), LocalDateTime.now());
 
         return new ResponseEntity<ErrorDetails>(error, HttpStatus.BAD_REQUEST);
+    }
+
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDetails> handleExceptionGlobally(Exception e) {
+        ErrorDetails error = new ErrorDetails("404 not FOUND", e.getMessage(), LocalDateTime.now());
+
+        return new ResponseEntity<ErrorDetails>(error, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
